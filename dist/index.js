aded49f03aecf82db1f3a6658dbd6436aebc7e27
kisleav <kisleav@users.noreply.github.com>
2022-10-02 15:53:29 +0300

    Delete dist directory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./cs-sdk-classic.cjs.production.min.js')
-} else {
-  module.exports = require('./cs-sdk-classic.cjs.development.js')
-}
